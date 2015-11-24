948e8a9ec2cac25988f868211e43f18f150ae636
James Oram <herespam1@hotmail.com>
2015-11-24 23:14:18 +0000

    make monitor run in new threads

--- bodhi.py
+++ bodhi.py
@@ -1,17 +1,22 @@
+"""Main Bodhi module
+"""
 import sys
+import thread
 from alerters.blink_alerter import BlinkAlerter
 from monitors.http_monitor import HttpMonitor
 from monitors.conf_loader import ConfLoader
 
 def main():
+    """main function
+    """
     if len(sys.argv) < 1:
-      print("Please specify configuration file(s)")
-      sys.exit(1)
+        print "Please specify configuration file(s)"
+        sys.exit(1)
 
     c = ConfLoader(sys.argv[1])
     c.load()
     mon = HttpMonitor(c.name, c.url, c.colour, c.poll_time, BlinkAlerter(), c.expected)
-    mon.monitor()
+    thread.start_new_thread(mon.monitor())
 
 if __name__ == "__main__":
     sys.exit(main())
