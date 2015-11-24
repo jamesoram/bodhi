1d809360758c45932702b8fa90d4d89c522d1bcd
James Oram <herespam1@hotmail.com>
2015-11-24 22:21:15 +0000

    fix example parameters

--- bodhi.py
+++ bodhi.py
@@ -3,7 +3,7 @@ from alerters.blink_alerter import BlinkAlerter
 from monitors.http_monitor import HttpMonitor
 
 def main():
-    mon = HttpMonitor("test", "http://localhost:31337", "hello", "DD2243", 40, BlinkAlerter())
+    mon = HttpMonitor("test", "http://localhost:31337", "DD2243", 40, BlinkAlerter(), "hello")
     mon.monitor()
 
 if __name__ == "__main__":
