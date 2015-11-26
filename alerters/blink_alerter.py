e75e22ba69e03241c8e42f331d16667f38023608
James Oram <herespam1@hotmail.com>
2015-11-26 21:42:21 +0000

    simplify alerting

--- alerters/blink_alerter.py
+++ alerters/blink_alerter.py
@@ -2,7 +2,6 @@
 """
 import threading
 import os
-import Queue
 import time
 
 class BlinkAlerter(threading.Thread):
@@ -11,24 +10,31 @@ class BlinkAlerter(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.queue = []
-        self.command = "blink1-tool --rgb "
+        self.command = "blink1-tool -q --rgb "
         self.blink_time = "95"
-        self.engaged = False
+        self.engaged = True
 
     def alert(self, monitor):
         """Alert using blink(1) with the configuration in `monitor`
         """
         self.queue.append(monitor)
+        print "Error in: " + monitor.name
         if self.engaged == False:
             self.engaged = True
-            print "Error in: " + monitor.name
             cmd = self.command + self.queue.pop().colour + " -q --blink " + self.blink_time
             os.system(cmd + "&")
         else:
-            os.system("blink1-tool --off")
-            for i in range(1, 10):
-                cmd = self.command + self.queue.pop().colour
-                os.system(cmd)
-                time.sleep(400)
-                os.system("blink1-tool --off")
-        self.engaged = False
+            os.system("blink1-tool -q --off")
+            timeout = time.time() + 35
+            while True:
+                for i in self.queue:
+                    cmd = self.command + i.colour
+                    os.system(cmd)
+                    time.sleep(0.8)
+                if time.time() > timeout:
+                    break
+            os.system("blink1-tool -q --off")
+            self.queue = []
+
+    def run(self):
+        time.sleep(0.1)
